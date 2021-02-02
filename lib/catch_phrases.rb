3a390171b762e91418f6a7bcc3d39203cf1e6339
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-02 22:18:02 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -11,3 +11,11 @@ end
 def link
   puts "It's Dangerous To Go Alone! Take This."
 end
+
+describe "all_phrases" do
+  it "puts out all of the previous catch phrases" do
+    expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
+    expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
+    expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
+  end
+end
